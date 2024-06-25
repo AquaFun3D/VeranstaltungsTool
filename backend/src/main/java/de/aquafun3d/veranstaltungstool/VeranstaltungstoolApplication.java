04ff50ad5f07016cffc9c47d8dfd6420a35e9b69
Nino Baumgart <nino@theska.de>
2024-06-25 10:40:54 +0200

    init Angular & Spring Boot

---
+++ backend/src/main/java/de/aquafun3d/veranstaltungstool/VeranstaltungstoolApplication.java
@@ -0,0 +1,13 @@
+package de.aquafun3d.veranstaltungstool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeranstaltungstoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeranstaltungstoolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/aquafun3d/veranstaltungstool/VeranstaltungstoolApplicationTests.java
@@ -0,0 +1,13 @@
+package de.aquafun3d.veranstaltungstool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeranstaltungstoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
